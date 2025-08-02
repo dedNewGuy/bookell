1d5e22d3542e8ceb860b7930ec6a57a2a422fbd3
dedNewGuy <aminkafrinewgen@gmail.com>
2025-08-02 16:43:25 +0800

    print file name and line count

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,23 @@
 module Main where
 
+import Data.List.Split
+
 import System.Environment
 import System.Exit
 
 processBookAt :: FilePath -> IO ()
 processBookAt filePath = do
+  -- Get the file Name
+  let fileName = last $ splitOn "/" filePath
+  putStrLn $ "Filename: " ++ fileName
+
+  -- Read the file content
   content <- readFile filePath
-  let contentLength = length $ lines content
-  print contentLength
+
+  -- Print the content line count
+  let contentLineCount = length $ lines content
+  putStrLn $ "Number of Lines: " ++ show contentLineCount
+
 
 main :: IO ()
 main = do
